991d404ff9f5c2df4149ef45e8eae89d83ec8985
Yan Kurhuzov <yan.dasty@gmail.com>
2023-10-05 21:17:17 +0300

    Commit 1: Initial commit

---
+++ src/main/java/goit/com/shorturlproject/ShortUrlProjectApplication.java
@@ -0,0 +1,13 @@
+package goit.com.shorturlproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortUrlProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortUrlProjectApplication.class, args);
+    }
+
+}
