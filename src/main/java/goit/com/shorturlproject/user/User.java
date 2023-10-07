a4d28799b7ae57c05aeae8c7d3dbe5f42066aeaa
Tanya Vynohradova <tanya.germanuk@gmail.com>
2023-10-07 22:20:28 +0300

    fix migration, add field in class UrlLink, removed annotation Data

--- src/main/java/goit/com/shorturlproject/user/User.java
+++ src/main/java/goit/com/shorturlproject/user/User.java
@@ -1,33 +1,46 @@
 package goit.com.shorturlproject.user;
 
 import goit.com.shorturlproject.url.UrlLink;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.OneToMany;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.annotation.Id;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
-@Data
 @RequiredArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
     private long id;
 
+    @Getter
+    @Setter
     private String userName;
 
+    @Getter
+    @Setter
     private String email;
 
+    @Getter
+    @Setter
     private String login;
 
+    @Getter
+    @Setter
     private String password;
 
     @OneToMany(mappedBy = "user")
     private List<UrlLink> links;
+
+    public void setLinks(List<UrlLink> links) {
+        this.links = links;
+        for (UrlLink link : links) {
+            link.setUser(this);
+        }
+    }
 }
--- src/main/resources/db/migration/V1__create_table_user.sql
+++ src/main/resources/db/migration/V1__create_table_user.sql
@@ -6,11 +6,13 @@ CREATE TABLE "user" (
     password VARCHAR(100) CHECK (LENGTH(password) >= 8 AND LENGTH(password) <= 100)
 );
 
-CREATE TABLE Url (
+CREATE TABLE url (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    longUrl VARCHAR(100000),
-    createdAt DATE,
-    clickTimes INTEGER
+    long_URL VARCHAR(100000),
+    short_URL VARCHAR(50),
+    created_at DATE,
+    click_times INTEGER,
+    expiration_date DATE,
     user_id INTEGER,
-    FOREIGN KEY (user_id) REFERENCES user (id)
+    FOREIGN KEY (user_id) REFERENCES "user" (id)
 );
