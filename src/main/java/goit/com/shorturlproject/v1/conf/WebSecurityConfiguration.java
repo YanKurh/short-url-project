e56f604970252388280fd045b484161f5e12de05
SlavaDef <slavadef@gmail.com>
2023-10-08 21:41:51 +0300

    first2

---
+++ src/main/java/goit/com/shorturlproject/v1/conf/WebSecurityConfiguration.java
@@ -0,0 +1,56 @@
+package goit.com.shorturlproject.v1.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity // turn on Security
+public class WebSecurityConfiguration {
+    private final DataSource dataSource;
+
+    public WebSecurityConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests((requests) -> {
+                            requests
+// permitAll() - дозвіл всім без регістрації дивитися сторінки при запиті методу GET
+                                    .requestMatchers(HttpMethod.GET, "/public").permitAll()
+                                    .anyRequest()
+                                    .authenticated(); // інші запити авторизуємо
+                        }
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/hello")
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception{
+        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username,password, enabled from users where username =?")
+                .authoritiesByUsernameQuery("select username, role from users where username =?");
+    }
+
+
+}
