fb36491674e908452b304d235f6bb3b952d806aa
Tanya Vynohradova <tanya.germanuk@gmail.com>
2023-10-08 20:25:55 +0300

    fix, name column in lower case

--- src/main/java/goit/com/shorturlproject/url/UrlLinkDTO.java
+++ src/main/java/goit/com/shorturlproject/url/UrlLinkDTO.java
@@ -4,7 +4,6 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Builder
 @RequiredArgsConstructor
 @AllArgsConstructor
 public class UrlLinkDTO {
@@ -26,30 +25,6 @@ public class UrlLinkDTO {
     @Getter
     @Setter
     private LocalDateTime expirationDate;
-
-    public static UrlLinkDTO fromUrlLink(UrlLink urlLink){
-
-        return UrlLinkDTO.builder()
-                .longUrl(urlLink.getLongUrl())
-                .shortUrl(urlLink.getShortUrl())
-                .createdAt(urlLink.getCreatedAt())
-                .clickTimes(urlLink.getClickTimes())
-                .expirationDate(urlLink.getExpirationDate())
-                .build();
-    }
-
-    public static UrlLink fromUrlLinkDTO(UrlLinkDTO urlLinkDto){
-
-        UrlLink url = new UrlLink();
-        url.setLongUrl(urlLinkDto.getLongUrl());
-        url.setShortUrl(urlLinkDto.getShortUrl());
-        url.setCreatedAt(urlLinkDto.getCreatedAt());
-        url.setClickTimes(urlLinkDto.getClickTimes());
-        url.setExpirationDate(urlLinkDto.getExpirationDate());
-
-        return url;
-
-    }
 
     public void setClickTimes(int clickTimes) {
         if (clickTimes < 0) {
--- src/main/java/goit/com/shorturlproject/user/User.java
+++ src/main/java/goit/com/shorturlproject/user/User.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
+import java.util.HashSet;
 
 @Entity
 @RequiredArgsConstructor
@@ -14,33 +14,34 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     @Getter
     @Setter
     private long id;
 
     @Getter
     @Setter
+    @Column(name = "user_name")
     private String userName;
 
     @Getter
     @Setter
+    @Column(name = "email")
     private String email;
 
     @Getter
     @Setter
+    @Column(name = "login")
     private String login;
 
     @Getter
     @Setter
+    @Column(name = "password")
     private String password;
 
     @OneToMany(mappedBy = "user")
-    private List<UrlLink> links;
+    @Getter
+    @Setter
+    private HashSet<UrlLink> links;
 
-    public void setLinks(List<UrlLink> links) {
-        this.links = links;
-        for (UrlLink link : links) {
-            link.setUser(this);
-        }
-    }
 }
