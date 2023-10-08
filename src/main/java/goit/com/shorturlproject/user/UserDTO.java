a6b925515e750d3aa76ce395ea41320eb74940dc
Tanya Vynohradova <tanya.germanuk@gmail.com>
2023-10-08 20:25:55 +0300

    fix, name column in lower case

--- src/main/java/goit/com/shorturlproject/user/UserDTO.java
+++ src/main/java/goit/com/shorturlproject/user/UserDTO.java
@@ -2,7 +2,6 @@ package goit.com.shorturlproject.user;
 
 import lombok.*;
 
-@Builder
 @RequiredArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
@@ -23,23 +22,5 @@ public class UserDTO {
     @Setter
     private String password;
 
-    public static UserDTO fromUser(User user){
-
-       return UserDTO.builder()
-               .userName(user.getUserName())
-               .email(user.getEmail())
-               .login(user.getLogin())
-               .password(user.getPassword())
-               .build();
-    }
-    public static User fromUserDto(UserDTO userDTO) {
-        User user = new User();
-        user.setUserName(userDTO.getUserName());
-        user.setEmail(userDTO.getEmail());
-        user.setLogin(userDTO.getLogin());
-        user.setPassword(userDTO.getPassword());
-
-        return user;
-    }
 
 }
