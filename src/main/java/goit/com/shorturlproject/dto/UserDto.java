b875f2569b66b69e0adf15676caacf4cdb65d303
Nikola2112 <nikola21122004@gmail.com>
2023-10-10 23:10:27 +0100

    check rules

--- src/main/java/goit/com/shorturlproject/dto/UserDto.java
+++ src/main/java/goit/com/shorturlproject/dto/UserDto.java
@@ -5,7 +5,9 @@ import goit.com.shorturlproject.validation.PasswordMatching;
 import goit.com.shorturlproject.validation.StrongPassword;
 import goit.com.shorturlproject.validation.ValidEmail;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -24,21 +26,26 @@ public class UserDto {
 
 
     @NotBlank
+    @NotEmpty
     @Size(max = 45)
     private String lastName;
 
     @NotBlank
+    @NotEmpty
     @Size(max = 30)
     private String firstName;
 
     @NotBlank
     @Size(max = 50)
     @ValidEmail
+    @NotEmpty(message = "Email can not be empty")
     private String email;
 
     @StrongPassword
+    @NotEmpty
     private String password;
 
+    @NotEmpty
     private String confirmPassword;
 
     private boolean isEnabled;
