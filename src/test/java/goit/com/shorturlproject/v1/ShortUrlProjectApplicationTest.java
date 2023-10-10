07527bbb51de3899ac0a550fc9e205f2238e275b
Yan Kurhuzov <yan.dasty@gmail.com>
2023-10-11 00:59:06 +0300

    Commit 15: fixed test problems with placeholders for main test class and added config.env for evn. variables

--- src/test/java/goit/com/shorturlproject/v1/ShortUrlProjectApplicationTest.java
+++ src/test/java/goit/com/shorturlproject/v1/ShortUrlProjectApplicationTest.java
@@ -1,9 +1,15 @@
 package goit.com.shorturlproject.v1;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("prod")
+@PropertySource("classpath:application-prod.properties")
 class ShortUrlProjectApplicationTest {
-//    @Test
+    @Test
     void mainTest() {
         ShortUrlProjectApplication.main(new String[]{});
     }
