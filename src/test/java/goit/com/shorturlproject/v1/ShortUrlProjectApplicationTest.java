0e918cfc7822b83c931feb6a9d2483abb75b210e
Yan Kurhuzov <yan.dasty@gmail.com>
2023-10-11 17:17:05 +0300

    Commit 16: fixed test problems

--- src/test/java/goit/com/shorturlproject/v1/ShortUrlProjectApplicationTest.java
+++ src/test/java/goit/com/shorturlproject/v1/ShortUrlProjectApplicationTest.java
@@ -2,13 +2,11 @@ package goit.com.shorturlproject.v1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("prod")
-@PropertySource("classpath:application-prod.properties")
-class ShortUrlProjectApplicationTest {
+class ShortUrlProjectApplicationTest implements ITestContainer {
     @Test
     void mainTest() {
         ShortUrlProjectApplication.main(new String[]{});
