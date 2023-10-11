2447072afab96ea2d53ae5d389205ec4acd5c707
Knotty123230 <122396662+Knotty123230@users.noreply.github.com>
2023-10-11 10:41:11 +0300

    fix

--- src/main/java/goit/com/shorturlproject/v1/user/repository/UserRepository.java
+++ src/main/java/goit/com/shorturlproject/v1/user/repository/UserRepository.java
@@ -4,9 +4,6 @@ import goit.com.shorturlproject.v1.user.dto.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findUserById(Long id);
 }
