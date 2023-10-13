cdd8c7ac55c91bad615c9a769ade96d89de3511a
Nikola2112 <nikola21122004@gmail.com>
2023-10-13 13:45:10 +0100

    registration done

--- src/main/java/goit/com/shorturlproject/v1/registration/validation/impl/StrongPasswordValidator.java
+++ src/main/java/goit/com/shorturlproject/v1/registration/validation/impl/StrongPasswordValidator.java
@@ -8,8 +8,7 @@ public class StrongPasswordValidator implements ConstraintValidator<StrongPasswo
 
   @Override
   public boolean isValid(String value, ConstraintValidatorContext context) {
-    // check if string contains at least one digit, one lowercase letter, one uppercase letter, one special character and 8 characters long
-    // return value.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!*()]).{8,}$");
+
     return value.matches("^[A-Z].*\\d$");
   }
 
