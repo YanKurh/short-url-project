b1e2d65f346ecd3fec55e518c67fb442f1508ec9
Knotty123230 <122396662+Knotty123230@users.noreply.github.com>
2023-10-16 14:01:38 +0300

    redis

--- src/test/java/goit/com/shorturlproject/v1/url/service/impl/UrlServiceImplTest.java
+++ src/test/java/goit/com/shorturlproject/v1/url/service/impl/UrlServiceImplTest.java
@@ -1,6 +1,5 @@
 package goit.com.shorturlproject.v1.url.service.impl;
 
-import goit.com.shorturlproject.v1.ITestContainer;
 import goit.com.shorturlproject.v1.url.dto.UrlLink;
 import goit.com.shorturlproject.v1.url.exceptions.UrlNotFoundException;
 import goit.com.shorturlproject.v1.url.repository.UrlRepository;
@@ -8,7 +7,9 @@ import goit.com.shorturlproject.v1.user.dto.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
@@ -18,20 +19,28 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+class UrlServiceImplTest {
 
-class UrlServiceImplTest implements ITestContainer {
+    private UrlServiceImpl urlService;
 
     @Mock
     private UrlRepository urlRepository;
 
-    private UrlServiceImpl urlService;
+    @Mock
+    private RedisTemplate<String, UrlLink> redisTemplate;
+
+    @Mock
+    private ValueOperations<String, UrlLink> valueOperations;
 
     @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-        urlService = new UrlServiceImpl(urlRepository);
-    }
+    void setUp() {
+        urlRepository = Mockito.mock(UrlRepository.class);
+        redisTemplate = Mockito.mock(RedisTemplate.class);
+        valueOperations = Mockito.mock(ValueOperations.class);
+        when(redisTemplate.opsForValue()).thenReturn(valueOperations);
 
+        urlService = new UrlServiceImpl(urlRepository, redisTemplate);
+    }
     @Test
     void testSaveAndFlush() {
         String shortUrl = "abc123";
@@ -53,32 +62,45 @@ class UrlServiceImplTest implements ITestContainer {
         assertEquals(expectedUrlLink, result);
     }
 
+
+
     @Test
     void testFindUrlLinkByLongUrl() {
-        String longUrl = "https://www.example.com";
-        UrlLink expectedUrlLink = new UrlLink();
-        expectedUrlLink.setLongUrl(longUrl);
+        String longUrl = "http://example.com";
+        UrlLink urlLink = new UrlLink();
 
-        when(urlRepository.findUrlLinkByLongUrl(longUrl)).thenReturn(Optional.of(expectedUrlLink));
+        when(urlRepository.findUrlLinkByLongUrl(longUrl)).thenReturn(Optional.of(urlLink));
 
-        Optional<UrlLink> result = urlService.findUrlLinkByLongUrl(longUrl);
+        Optional<UrlLink> foundUrlLink = urlService.findUrlLinkByLongUrl(longUrl);
 
-        assertTrue(result.isPresent());
-        assertEquals(expectedUrlLink, result.get());
+        assertTrue(foundUrlLink.isPresent());
+        assertEquals(urlLink, foundUrlLink.get());
     }
 
     @Test
     void testFindUrlLinkByShortUrl() {
-        String shortUrl = "abc123";
-        UrlLink expectedUrlLink = new UrlLink();
-        expectedUrlLink.setShortUrl(shortUrl);
+        String shortUrl = "short-url";
+        UrlLink urlLink = new UrlLink();
 
-        when(urlRepository.findUrlLinkByShortUrl(shortUrl)).thenReturn(Optional.of(expectedUrlLink));
 
-        UrlLink result = urlService.findUrlLinkByShortUrl(shortUrl);
+        when(valueOperations.get(shortUrl)).thenReturn(urlLink);
 
-        assertEquals(expectedUrlLink, result);
+        UrlLink foundUrlLink = urlService.findUrlLinkByShortUrl(shortUrl);
+
+        assertEquals(urlLink, foundUrlLink);
     }
+
+    @Test
+    void testFindUrlLinkByShortUrlUrlNotFoundException() {
+        String shortUrl = "non-existent-short-url";
+
+        when(valueOperations.get(shortUrl)).thenReturn(null);
+
+        assertThrows(UrlNotFoundException.class, () -> urlService.findUrlLinkByShortUrl(shortUrl));
+    }
+
+
+
 
     @Test
     void testFindUrlLinkByShortUrlNotFound() {
